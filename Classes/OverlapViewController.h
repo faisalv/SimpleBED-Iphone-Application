1915e7aa6e8626f549be8526a42c00b6f2109b12
Faisal Vali <faisalv@gmail.com>
2011-09-11 22:03:01 -0500

    cleaned out commented extraneous code

---
+++ Classes/OverlapViewController.h
@@ -0,0 +1,13 @@
+//
+//  OverlapViewController.h
+//  SimpleBEDCalc
+//
+//  Created by Faisal Vali on 9/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverlapViewController : UIViewController
+
+@end
